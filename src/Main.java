a95bda5f7cb3d941bb7e6538080bac8109c762a4
wesley-nunes-00@hotmail.com <wesley-nunes-00@hotmail.com>
2023-02-27 22:03:25 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        System.out.println(name);
+
     }
 }
